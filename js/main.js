7b6022e03e6d7a235ef1c44834dd1c7b46606052
AndreaDuh <xAsianRejectx@gmail.com>
2015-11-09 03:54:00 -0800

    Added styling for non interactive version

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	var a = new Array(4);
+	a[0] = "Who was the original killer for the Friday the 13th films?";
+	a[1] = "Michael Myers";
+	a[2] = "Jason Voorhees";
+	a[3] = "Freddy Krueger";
+	a[4] = "Pamela Voorhees";
+});
