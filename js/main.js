0fce51a1e871a3c10714d6af3d57bfbf638a5ea1
AndreaDuh <xAsianRejectx@gmail.com>
2015-11-12 04:29:46 -0800

    Created consructed objects for questions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,65 @@
 $(document).ready(function () {
-	var a = new Array(4);
-	a[0] = "Who was the original killer for the Friday the 13th films?";
-	a[1] = "Michael Myers";
-	a[2] = "Jason Voorhees";
-	a[3] = "Freddy Krueger";
-	a[4] = "Pamela Voorhees";
+	var quiz = new Obeject();
+	quiz.question = "Who was the original killer in the Friday the 13th films?";
+	quiz.answer1 = "1. Michael Myers";
+	quiz.answer2 = "2. Jason Voorhees";
+	quiz.answer3 = "3. Fredddy Krueger";
+	quiz.answer3 = "4. Pamela Voorhees"
+	quiz.correct = "4. Pamela Voorhees";
+
+	var quiz = new Obeject();
+	quiz.question = "Who did Johnny Depp play in the hit slasher Nightmare on Elm Street?";
+	quiz.answer1 = "1. Josh";
+	quiz.answer2 = "2. Glenn";
+	quiz.answer3 = "3. Bobby";
+	quiz.answe4 = "4. Freddy";
+	quiz.correct = "2. Glenn";
+
+	var quiz = new Obeject();
+	quiz.question = "Which one of these ladies is not considered to be a final girl?";
+	quiz.answer1 = "1. Sidney Prescott";
+	quiz.answer2 = "2. Laurie Strode";
+	quiz.answer3 = "3. Selina Kyle";
+	quiz.answer3 = "4. Nancy Thompson";
+	quiz.correct = "3. Selina Kyle";
+
+	var quiz = new Obeject();
+	quiz.question = "Accoring to Jamie Kennedy's Scream character, Randy, which one of these is a rule to survive a horrr movie?";
+	quiz.answer1 = "1. Don't have sex";
+	quiz.answer2 = "2. Never walk alone anywhere";
+	quiz.answer3 = "3. Grab the most lethal weapon yiu can find";
+	quiz.answer3 = "4. Hide and call the cops";
+	quiz.correct = "1. Don't have sex";
+
+	var quiz = new Obeject();
+	quiz.question = "There is only one thing that can break Dracula's trance spell, what is it?";
+	quiz.answer1 = "1. Garlic";
+	quiz.answer2 = "2. True Love";
+	quiz.answer3 = "3. Stabbing Dracula with a wooden stake";
+	quiz.answer3 = "4. Burning his coffin";
+	quiz.correct = "2. True Love";
+
+	var quiz = new Obeject();
+	quiz.question = "What demon was Reagan possessed by in the Exorcist movie?";
+	quiz.answer1 = "1. Balthazr";
+	quiz.answer2 = "2. Lucifer";
+	quiz.answer3 = "3. Azazel";
+	quiz.answer3 = "4. Pazuzu";
+	quiz.correct = "4. Pazuzu";
+
+	var quiz = new Obeject();
+	quiz.question = "Michael Myers usually uses what weapon to kill his victoms?";
+	quiz.answer1 = "1. Kitchen Knife";
+	quiz.answer2 = "2. Chainsaw";
+	quiz.answer3 = "3. Hand with sharp knives attached to it";
+	quiz.answer3 = "4. Machete";
+	quiz.correct = "1. Kitchen Knife";
+
+	var quiz = new Obeject();
+	quiz.question = "Leatherface of The Texas Chainsaw franchise is based off what real life serial killer?";
+	quiz.answer1 = "1. John Wayne Gacy";
+	quiz.answer2 = "2. Henry Haward Holmes";
+	quiz.answer3 = "3. Ed Gein";
+	quiz.answer3 = "4. Ted Bundy";
+	quiz.correct = "3. Ed Gein";
 });
